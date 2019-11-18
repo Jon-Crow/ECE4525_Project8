15b0ecdaa59a6e2fa5e5fde35007f4b5ae8ab082
jon-crow <cjonat1@vt.edu>
2019-11-18 17:15:51 -0500

    w.o.w.

--- project8_cjonat1.js
+++ project8_cjonat1.js
@@ -30,7 +30,7 @@ var sketchProc = function(processingInstance)
 		var edges = [[0, 1], [1, 3], [3, 2], [2, 0],
 					[4, 5], [5, 7], [7, 6], [6, 4],
 					[0, 4], [1, 5], [2, 6], [3, 7]];
-		var faces = [[0,2,6,4],[0,1,3,2],[0,1,5,4]];
+		var faces = [[4,6,7,5],[2,3,7,6],[1,3,7,5]]; //[0,2,6,4],[0,1,3,2],[0,1,5,4],
 		return { 'nodes': nodes, 'edges': edges, 'faces' : faces };
 	};
 
